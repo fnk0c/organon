ed93dce3b92b1b07ad316427a42a58a90bb7fbf8
Franco Colombino <fnk0c@users.noreply.github.com>
2015-02-20 14:50:24 -0200

    Fix ruby compilation
    Necessário libffi-dev para compilar

--- install.sh
+++ install.sh
@@ -41,6 +41,7 @@ rubyversion=`ruby --version | cut -f1 -d. | grep -i "$ruby 2"`
 if [ ! -n "$rubyversion" ]
 then
 	echo "Installing Ruby 2"
+	apt-get install libffi-dev
 	wget http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.0.tar.gz
 	tar xpvf ruby-2.2.0.tar.gz
 	cd ruby-2.2.0 && ./configure && make && make install
