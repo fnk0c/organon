b55649f958214f4db79471e12501bdcf83c0a923
fnk0c <franco.c.colombino@gmail.com>
2015-02-22 15:40:52 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -45,10 +45,14 @@ then
 	wget http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.0.tar.gz
 	tar xpvf ruby-2.2.0.tar.gz
 	cd ruby-2.2.0 && ./configure && make && make install
-	cd .. && cd ..
+	cd ..	#Exits ruby-2.2.0 directory
 else
 	echo " [!] ruby 2 installed"
 fi
+
+#End Ruby check
+
+cd .. 	#Exits organon directory
 
 gem install mysql colorize
 
