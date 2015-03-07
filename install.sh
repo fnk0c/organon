d7d3e8f24e89836e42d08d59f8bfcf4aff093e7d
Ygor Máximo <maximozsec@outlook.com.br>
2015-03-07 20:36:34 -0300

    Changed the permission user to execute install.sh
    added `bundler` dependence

--- install.sh
+++ install.sh
@@ -4,22 +4,15 @@
 #Author = Fnkoc
 #Project = https://github.com/maximozsec/organon
 
-#Check user permition
-if [ "$(id -u)" != "0" ]; then
-	clear
-	echo -e " \033[31m [!]\033[00m This script must be run as root " 1>&2
-	exit
-fi
-
-#Check if already installed
+#Check if is already installed
 if [ -e /opt/organon ]
 then
-	rm -rf /opt/organon
+	sudo rm -rf /opt/organon
 fi
 
 if [ -e /usr/bin/organon ]
 then
-	rm /usr/bin/organon
+	sudo rm /usr/bin/organon
 fi
 
 #end check
@@ -30,47 +23,36 @@ rubyversion=`ruby --version | cut -f1 -d. | grep -i "$ruby 2"`
 if [ ! -n "$rubyversion" ]
 then
 	echo -e "\033[31m [!]\033[00m Installing Ruby 2"
-	apt-get install libffi-dev libssl-dev libreadline-dev make g++ build-essential zlib1g-dev libyaml-dev
+	sudo apt-get install libffi-dev libssl-dev libreadline-dev make g++ build-essential zlib1g-dev libyaml-dev
 	wget http://cache.ruby-lang.org/pub/ruby/2.2/ruby-2.2.0.tar.gz
 	tar xpvf ruby-2.2.0.tar.gz
-	cd ruby-2.2.0 && ./configure && make && make install
+	cd ruby-2.2.0 && ./configure && make && sudo make install
 	cd ..	#Exits ruby-2.2.0 directory
 else
 	echo -e "\033[31m [!]\033[00m ruby 2 installed"
 fi
 #End Ruby check
 
-#Install Dependencies
-#sys=`cat /etc/issue | grep -i ubuntu`
-
-#if [ ! -n "$sys" ]
-#then
-#	apt-get install python libmysqlclient-dev rubygems
-#else
-#	echo -e "\033[32m [+]\033[00m UBUNTU OS"
-#	apt-get install python libmysqlclient-dev rubygems-integration
-#fi
-apt-get install python libmysqlclient-dev
+sudo apt-get install python libmysqlclient-dev bundler
+bundle install
 
 cd .. 	#Exits organon directory
 
-gem install mysql colorize
-
 #Move organon to opt
 echo -e "\033[32m [+]\033[00m Moving organon to /opt"
-mv organon /opt/
+sudo mv organon /opt/
 
 echo -e "\033[32m [+]\033[00m Creating symbolic link"
-echo \#\!/bin/bash >> /usr/bin/organon
-echo cd /opt/organon >> /usr/bin/organon
-echo exec python organon.py \"\$\@\" >> /usr/bin/organon
+sudo sh -c "echo \#\!/bin/bash >> /usr/bin/organon"
+sudo sh -c "echo cd /opt/organon >> /usr/bin/organon"
+sudo sh -c "echo exec python organon.py \"\$\@\" >> /usr/bin/organon"
 
 echo -e "\033[32m [+]\033[00m Changing permitions"
-chmod +x /usr/bin/organon
-chmod +x /opt/organon/src/*
+sudo chmod +x /usr/bin/organon
+sudo chmod +x /opt/organon/src/*
 
 echo -e "\033[32m [+]\033[00m Cleaning directory"
-rm -rf /opt/organon/ruby-2.2.0*
+sudo rm -rf /opt/organon/ruby-2.2.0*
 
 echo -e '\033[32m [+]\033[00m Complete!'
 echo 'Type "organon" in order to use it'
