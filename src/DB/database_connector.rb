e0f26886f91018b97f4eb21516dd2a0ea25eceef
Ygor Máximo <maximozsec@outlook.com.br>
2015-04-04 05:53:51 -0300

    Fix depedence's bug

--- src/DB/database_connector.rb
+++ src/DB/database_connector.rb
@@ -53,35 +53,29 @@ class Resp
 				puts pkgconf
 				system pkgconf
 
-				if row[1] != nil # Checking if the row of the dependencies of the given tool is not empty
-
-					dep = "sudo apt-get install #{row[1]} -y"
-					
-					def inst_dep
-						puts " [" + "!".red + "] Installing dependencies......."
-						system dep
-					end
-
-					display = -> do # Proc method to behave as a function and facilitate its recursive calling
-						puts "The following dependencies are necessary for this tool."
-						puts "(#{row[1].split.length}) #{row[1]}"
-						puts "Would you like to install them? (Y/n)"
-						print "=> "
-						opt = STDIN.gets.chomp
-							case opt
-								when /[Yy]/ 
-									inst_dep
-								when /[Nn]/
-									exit
-								else
-									puts "Thats not an option, buddy."
-									display.call
-							end
-					end
-					display.call
-				else
-					puts "[" + "~".blue + "] No necessary dependence"
-				end
+				if row[1] != 'NULL' # Checking if the row of the dependencies of the given tool is not empty
+ 
+                    display = -> do # Proc method to behave as a function and facilitate its recursive calling
+                        puts "The following dependencies are necessary for this tool."
+                        puts "(#{row[1].split.length}) #{row[1]}"
+                        puts "Would you like to install them? (Y/n)"
+                        print "=> "
+                        opt = STDIN.gets.chomp
+                                case opt
+                                    when /[Yy]/
+                                        puts " [" + "!".red + "] Installing dependencies........"
+                                        system "sudo apt-get install #{row[1]} -y"
+                                    when /[Nn]/
+                                        exit 1
+                                    else
+                                        puts " [" + "!".red + "] Thats not an option, buddy.\n"
+                                        display.call
+                                end
+                	end
+                	display.call
+                else
+                    puts "[" + "~".blue + "] No necessary dependence"
+                end
 			end
 		rescue Exception
 			exit!			# Exiting immediately
